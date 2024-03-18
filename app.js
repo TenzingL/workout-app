02d006b7ed4ac471df49b9845a85758a6a5a2f50
Tenzing Lama <tenzingnyimalama@gmail.com>
2024-03-17 21:10:04 -0400

    Allowed CSS to be used with HBS filetypes

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const handlebars = require('hbs');
 const express = require('express');
 const app = express();
+app.use(express.static('css'))
 app.listen(3000, () => {
     console.log('listening on port 3000')
+})
+
+app.get("/", (req, res) => {
+    res.render("index.hbs")
 })
